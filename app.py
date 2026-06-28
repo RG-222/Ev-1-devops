0d5a32c40618fa4ebf6d2563d7c709893416b7b4
RG-222 <re.gonzaleza@duocuc.cl>
2026-06-28 19:46:23 -0400

    fix: añadir excepción controlada #nosec para permitir bind seguro en docker

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def calcular_suma(a, b):
 
 if __name__ == '__main__':
     # El servidor escucha en todas las interfaces en el puerto 5000
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)  # nosec
